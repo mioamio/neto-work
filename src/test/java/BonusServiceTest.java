72dff092964294c7613a5cc90353edf4f757d95c
mioamio <zwv0101@gmail.com>
2022-09-17 11:19:39 +0300

    add tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -3,13 +3,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BonusServiceTest {
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    void shouldCalculateForRegisteredAndUnderLimit() { //следует Рассчитать Для Зарегистрированных И Находящихся До Лимита
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000;
-        boolean registered = true;
-        long expected = 30;
+        long amount = 1000; // сумма покупки
+        boolean registered = true; // статус пользователя
+        long expected = 30; // ожидаемый результат
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -19,13 +19,93 @@ public class BonusServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndOverLimit() {
+    void shouldCalculateForRegisteredAndOverLimit() { // следует Рассчитать Для Зарегистрированных И Превышающих Лимит
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
         long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shoulBeCalculatedForunregisteredAndUnderLimit() { //следует Рассчитать Для неЗарегистрированных И Находящихся До Лимита
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000; // сумма покупки
+        boolean registered = false; // статус пользователя
+        long expected = 10; // ожидаемый результат
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void ItshouldBecalculatedForunregisteredAndExceedingLimit() { // следует Рассчитать Для неЗарегистрированных И Превышающих Лимит
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndUnderLimitborder_1() { //граница лимита ==
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 16667; // сумма покупки
+        boolean registered = true; // статус пользователя
+        long expected = 500; // ожидаемый результат
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndUnderLimitborder_2() { //граница лимита -1
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 16666; // сумма покупки
+        boolean registered = true; // статус пользователя
+        long expected = 499; // ожидаемый результат
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndUnderLimitborder_3() { //граница лимита +1
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 16668; // сумма покупки
+        boolean registered = true; // статус пользователя
+        long expected = 500; // ожидаемый результат
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
